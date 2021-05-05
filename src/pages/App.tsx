9920695ad3bb005fe936efca3e5294eeafec3157
Robin <irobinz55@gmail.com>
2021-05-06 00:22:24 +0700

    init

---
+++ src/pages/App.tsx
@@ -0,0 +1,100 @@
+import React, { Suspense, useEffect, useState } from 'react'
+import {  Route, Switch, Router } from 'react-router-dom'
+import styled from 'styled-components'
+import { Credentials, StringTranslations } from '@crowdin/crowdin-api-client'
+import { LangType } from '@pancakeswap-libs/uikit'
+import { createBrowserHistory } from 'history'
+
+import Popups from '../components/Popups'
+import Web3ReactManager from '../components/Web3ReactManager'
+import Swap from './Swap'
+
+import { EN, allLanguages } from '../constants/localisation/languageCodes'
+import { LanguageContext } from '../hooks/LanguageContext'
+import { TranslationsContext } from '../hooks/TranslationsContext'
+
+import Menu from '../components/Menu'
+import useGetDocumentTitlePrice from '../hooks/useGetDocumentTitlePrice'
+
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  overflow-x: hidden;
+`
+
+const BodyWrapper = styled.div`
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 1;
+  margin-bottom: 64px;
+  ${({ theme }) => theme.mediaQueries.lg} {
+    margin-bottom: 0;
+  }
+`
+
+const CACHE_KEY = 'pancakeSwapLanguage'
+const customHistory = createBrowserHistory();
+
+export default function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState<any>(undefined)
+  const [translatedLanguage, setTranslatedLanguage] = useState<any>(undefined)
+  const [translations, setTranslations] = useState<Array<any>>([])
+
+  const getStoredLang = (storedLangCode: string) => {
+    return allLanguages.filter((language) => {
+      return language.code === storedLangCode
+    })[0]
+  }
+
+  useEffect(() => {
+    const storedLangCode = localStorage.getItem(CACHE_KEY)
+    if (storedLangCode) {
+      const storedLang = getStoredLang(storedLangCode)
+      setSelectedLanguage(storedLang)
+    } else {
+      setSelectedLanguage(EN)
+    }
+  }, [])
+
+
+
+
+  const handleLanguageSelect = (langObject: LangType) => {
+    setSelectedLanguage(langObject)
+    localStorage.setItem(CACHE_KEY, langObject.code)
+  }
+
+  useGetDocumentTitlePrice()
+
+  return (
+    <Suspense fallback={null}>
+      <Router history={customHistory}>
+        <AppWrapper>
+          <LanguageContext.Provider
+            value={{
+              selectedLanguage,
+              setSelectedLanguage: handleLanguageSelect,
+              translatedLanguage,
+              setTranslatedLanguage,
+            }}
+          >
+            <TranslationsContext.Provider value={{ translations, setTranslations }}>
+              <Menu>
+                <BodyWrapper>
+                  <Popups />
+                  <Web3ReactManager>
+                    <Switch>
+                      <Route component={Swap}/>
+                    </Switch>
+                  </Web3ReactManager>
+                </BodyWrapper>
+              </Menu>
+            </TranslationsContext.Provider>
+          </LanguageContext.Provider>
+        </AppWrapper>
+      </Router>
+    </Suspense>
+  )
+}
